a1e0f4d6686afa0975442760e9dccb0052a1628a
sein0573 <144101162+sein0573@users.noreply.github.com>
2026-02-03 15:55:00 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ const nav = document.querySelector('.nav');
 toggleBtn.addEventListener('click', () => {
 nav.classList.toggle('active'); // 🔹 active 클래스 토글
 });
+
+const cards = document.querySelectorAll('.card');
+const modal = document.getElementById('modal');
+const title = document.getElementById('modal-title');
+const desc = document.getElementById('modal-desc');
+const img = document.getElementById('modal-img');
+const role = document.getElementById('modal-role');
+
+const closeBtn = document.querySelector('.close');
+
+cards.forEach(card=>{
+  card.addEventListener('click', ()=>{
+    title.textContent = card.dataset.title;
+    desc.textContent = card.dataset.desc;
+    img.src = card.dataset.img;
+    role.textContent = "사용 기술 : " + card.dataset.role;
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', ()=>{
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e)=>{
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+});
